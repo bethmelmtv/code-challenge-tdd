b86e12ea2ec108e6bdd8c8a2540fc9d871e653f8
Beth <bethmelmtv@gmail.com>
2022-03-10 13:06:55 -0800

    tested two functions

--- test/functions.test.js
+++ test/functions.test.js
@@ -1,24 +1,60 @@
 // IMPORT MODULES under test here:
 import { 
-    myFunction,
+    addExclamationPoints, 
+    divideThenMultiply, 
+    getLastItem, 
+    getRandomNumber, 
+    getSecondItem, 
+    makeLuckyGreeting, 
+    multiplyBy12ThenHalve, 
+    multiplyBySeven, 
+    returnAsAnArray, 
+    returnAsAString,
 } from '../functions.js';
 
 const { test, skip } = QUnit;
 
 test('this test should pass', (expect) => {
-    const expected = true;
+    const expected = 'bunny rabbit!!!';
 
-    const actual = myFunction();
+    const actual = addExclamationPoints('bunny rabbit');
 
     expect.equal(actual, expected);
 });
 
 
-skip('this test should be skipped', (expect) => {
-    const expected = true;
+test('this test should pass', (expect) => {
+    const expected = 28;
+
+    const actual = multiplyBySeven(4);
+
+    expect.equal(actual, expected);
+});
+
+
+test('this test should pass', (expect) => {
+    const expected = 24;
+
+    const actual = multiplyBy12ThenHalve(4);
+
+    expect.equal(actual, expected);
+});
+
+
+
+
+test('function multiplyBy12ThenHalve(num1, num2, num3)', (expect) => {
+    const expected = (num1/num2) * num3;
 
     const actual = true;
 
     expect.equal(actual, expected);
+});
+
+test('function divideThenMultiply(firstNumber, secondNumber, thirdNumber))', (expect) => {
+    const expected = [firstNumber,secondNumber,thirdNumber]
+    const actual = true;
+
+    expect.deepEqual(actual, expected);
 });
 
